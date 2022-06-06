6165bfc15d36c4c8d2299dc4bf60aec233ca7ff4
Yiğit Cemal Öztürk <69117901+CadmiumC4@users.noreply.github.com>
2022-06-06 11:23:53 +0300

    Initial Commit
    Added the first two dummies (Base64 CLI and Random Color)

---
+++ base64.ps1
@@ -0,0 +1,5 @@
+﻿param([String]$Text)
+
+$Bytes = [System.Text.Encoding]::Unicode.GetBytes($Text)
+$EncodedText =[Convert]::ToBase64String($Bytes)
+Write-Host $EncodedText
---
+++ colors.ps1
@@ -0,0 +1,12 @@
+﻿$colors = @(
+            "Black", "Blue", "Cyan",
+            "DarkBlue", "DarkCyan", "DarkGray",
+            "DarkGreen", "DarkMagenta", "DarkRed",
+            "DarkYellow", "Gray", "Green", 
+            "Magenta", "Red", "Yellow", "White"
+            )
+$textColor = $colors[(Get-Random -Minimum 0 -Maximum 15)]
+
+Write-Host "I am written in " -NoNewline
+Write-Host -ForegroundColor $textColor $textColor -NoNewline
+Write-Host "!"
